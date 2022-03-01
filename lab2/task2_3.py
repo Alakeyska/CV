defd9f47c6fa286d40f91bcebd700b9e874d2bc9
Alakey <Salamakhin67@gmail.com>
2022-03-02 01:06:24 +0300

    video rec off

--- lab2/task2_3.py
+++ lab2/task2_3.py
@@ -4,24 +4,24 @@ path = 'S:\\CV\\lab2\\lab2-4.mp4'
 out_path = 'S:\\CV\\lab2\\out.mp4'
 
 cap = cv2.VideoCapture(path)
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(out_path, -1, 30.0, (frame_width, frame_height))
+# # Video REC
+# frame_width = int(cap.get(3))
+# frame_height = int(cap.get(4))
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# out = cv2.VideoWriter(out_path, -1, 30.0, (frame_width, frame_height))
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ret_otsu, thresh = cv2.threshold(gray, 111, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        out.write(thresh)
-        # cv2.imshow('frame', thresh)
-        # print(ret_otsu)
+        # out.write(thresh)
+        cv2.imshow('frame', thresh)
+        print(ret_otsu)
         if cv2.waitKey(1) == ord('q'):
             break
     else:
         break
 cap.release()
-out.release()
-
 cv2.destroyAllWindows()
+# out.release()
