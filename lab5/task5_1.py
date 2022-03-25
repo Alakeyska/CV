5ee1be745656e951ea735ff2c54b7b71379d56f1
Alakey <Salamakhin67@gmail.com>
2022-03-25 12:31:29 +0300

    added print_images

--- lab5/task5_1.py
+++ lab5/task5_1.py
@@ -2,7 +2,18 @@
 import cv2
 from matplotlib import pyplot as plt
 
-i = 0
+
+def print_images(dict, fig_name, row, col):
+    i = 0
+    for title, image in dict.items():
+        fig_name.add_subplot(row, col, i + 1)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        plt.imshow(image)
+        plt.title(title)
+        plt.axis('off')
+        i += 1
+
+
 path_cage = 'S:\\CV\\lab5\\cage_lines.jpeg'
 path_oblique = 'S:\\CV\\lab5\\oblique_lines.jpg'
 img_cage = cv2.imread(path_cage, cv2.IMREAD_REDUCED_GRAYSCALE_4)
@@ -20,31 +31,14 @@ sobel = {'img_cage': img_cage, 'sobel_img_cage_y1': sobel_img_cage_y1, 'sobel_im
          'sobel_img_oblique_x1': sobel_img_oblique_x1}
 
 fig1 = plt.figure('sobel', figsize=(10, 7))
-for title, image in sobel.items():
-    fig1.add_subplot(2, 3, i + 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    plt.imshow(image)
-    plt.title(title)
-    plt.axis('off')
-    i += 1
-    # cv2.imshow(title, image)
-i = 0
+print_images(sobel, fig1, 2, 3)
 
 # laplacian
 laplace_img_cage = cv2.Laplacian(img_cage, -1, ksize=3)
 laplace_img_oblique = cv2.Laplacian(img_oblique, -1, ksize=3)
 
-
 laplace = {'img_cage': img_cage, 'laplace_img_cage': laplace_img_cage, 'img_oblique': img_oblique,
            'laplace_oblique': laplace_img_oblique}
 fig2 = plt.figure('laplace', figsize=(10, 7))
-for title, image in laplace.items():
-    fig2.add_subplot(2, 2, i + 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    plt.imshow(image)
-    plt.title(title)
-    plt.axis('off')
-    i += 1
-    # cv2.imshow(title, image)
+print_images(laplace, fig2, 2, 2)
 plt.show()
-i = 0
